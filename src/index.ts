db59d52801c5f44082c579ccf68cd5c6ce7be80b
Florian Ludot <florian.ludot@gmail.com>
2022-08-27 21:14:03 +0900

    fix: getInput test

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
-import { debug, getInput, setFailed } from '@actions/core';
+import * as core from '@actions/core';
 import notify from './notify';
 
 async function run(): Promise<void> {
-    const accessToken = getInput('INPUT_ACCESS_TOKEN');
-    const threadKey = getInput('INPUT_THREAD_KEY');
-    const text = getInput('INPUT_TEXT');
+    const accessToken = core.getInput('access-token');
+    const threadKey = core.getInput('thread-key');
+    const text = core.getInput('text');
 
+    core.debug(accessToken);
     try {
         const res = await notify(accessToken, threadKey, text);
-        debug(JSON.stringify(res));
+        core.debug(JSON.stringify(res));
     } catch (e) {
         if (e instanceof Error) {
-            setFailed(e.message);
+            core.setFailed(e.message);
         }
     }
 }
