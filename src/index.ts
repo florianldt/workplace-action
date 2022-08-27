bbf480eda153e22d0260190ac83f6cb45093190e
Florian Ludot <florian.ludot@gmail.com>
2022-08-27 20:26:16 +0900

    fix: action variable names

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@ import { debug, getInput, setFailed } from '@actions/core';
 import notify from './notify';
 
 async function run(): Promise<void> {
-    const accessToken = getInput('access-token');
-    const threadKey = getInput('thread-key');
-    const text = getInput('text');
+    const accessToken = getInput('INPUT_ACCESS_TOKEN');
+    const threadKey = getInput('INPUT_THREAD_KEY');
+    const text = getInput('INPUT_TEXT');
 
     try {
         const res = await notify(accessToken, threadKey, text);
