e674888d2d3e5ae1702f7d8ad65e2cf801a17348
Florian Ludot <florian.ludot@gmail.com>
2022-08-27 17:49:37 +0900

    feat: implement base action

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,19 @@
-// eslint-disable-next-line no-console -- TODO
-console.log('workplace-action');
+import { debug, getInput, setFailed } from '@actions/core';
 
-export {};
+import notify from './notify';
+
+async function run(): Promise<void> {
+    const accessToken = getInput('bot_access_token');
+    const threadKey = getInput('thread_key');
+
+    try {
+        const res = await notify(accessToken, threadKey);
+        debug(JSON.stringify(res));
+    } catch (e) {
+        if (e instanceof Error) {
+            setFailed(e.message);
+        }
+    }
+}
+
+run();
