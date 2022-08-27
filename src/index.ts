22123883e2a11e41155176a5e986bf890721b357
Florian Ludot <florian.ludot@gmail.com>
2022-08-27 19:15:17 +0900

    feat: update action input names

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import { debug, getInput, setFailed } from '@actions/core';
 import notify from './notify';
 
 async function run(): Promise<void> {
-    const accessToken = getInput('bot_access_token');
-    const threadKey = getInput('thread_key');
+    const accessToken = getInput('bot-access-token');
+    const threadKey = getInput('thread-key');
     const text = getInput('text');
 
     try {
